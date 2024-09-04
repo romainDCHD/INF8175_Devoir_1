10219792d4132bf265677721f877a73ed5a8ffb7
romainDCHD <romain.duchadeau2001@gmail.com>
2024-09-04 17:14:45 -0400

    Ajout algo DFS non fonctionel

--- code/search.py
+++ code/search.py
@@ -99,6 +99,37 @@ def depthFirstSearch(problem:SearchProblem)->List[Direction]:
     '''
         INSÉREZ VOTRE SOLUTION À LA QUESTION 1 ICI
     '''
+    
+    print("Start:", problem.getStartState())
+    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
+    print("Start's successors:", problem.getSuccessors(problem.getStartState()))   
+    
+    L = util.Stack()    # Initialisation LIFO
+    L.push((problem.getStartState(), []))   # Ajout du premier état dans la LIFO, comment y aller 
+    visited = [] # La memoire qui permet de retenir ou on est allés pour pas y retourner
+    path = []   # Solution finale
+        
+    while not L.isEmpty():
+    #for i in range (0,10):
+        print("L avant pop", L[0])
+        state, path = L.pop() # Exploration du dernier état et on l'enlevede la LIFO
+        print("state", state)
+        print("L apres pop", L[0])
+        print("solution intermediaire", path)
+        
+        # On regarde si on est dans un état final
+        if (problem.isGoalState(state[0]) == True ):
+            print("\nSolution finale: ", path)
+            return path
+        
+        else :
+            visited.append(state)
+            print("visited", visited)
+            # solution.pop()  # On s'était trompés donc on enlève le mouvement de la solution 
+            for successor, action, _ in problem.getSuccessors(state):
+                if successor not in visited:
+                    L.push((successor, path + [action]))
+                 
 
     util.raiseNotDefined()
 
